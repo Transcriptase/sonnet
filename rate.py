239d7c460d4382d3b295052f2e75642265b073fd
Russell Williams <russell.d.williams@gmail.com>
2016-04-30 19:51:29 -0400

    Small collection and template adjustments

--- rate.py
+++ rate.py
@@ -2,7 +2,7 @@ import pickle
 import sonnet as snt
 
 
-with open("unrated_batch_01.pickle", "r") as f:
+with open("unrated_batch_02.pickle", "r") as f:
 	all_sonnets = pickle.load(f)
 
 rated_sonnets = []
@@ -12,5 +12,6 @@ for sonnet in all_sonnets:
 		section.get_user_rating()
 	rated_sonnets.append(sonnet)
 
-with open("rated_batch_01.pickle", "wb") as f:
+
+with open("rated_batch_02.pickle", "wb") as f:
 	pickle.dump(rated_sonnets, f)
