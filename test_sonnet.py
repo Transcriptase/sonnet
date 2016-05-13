2eba503bf9caa3579985d9f4caf03f10defbc9fb
Russell <ruwilliams@genedx.com>
2016-05-13 11:31:05 -0400

    Fixed tests for new common threshold

--- test_sonnet.py
+++ test_sonnet.py
@@ -226,7 +226,7 @@ class TestLine(object):
 class TestTemplate(object):
     def test_list_maker(self):
         nouns = vocab.common_words("NN")
-        eq_(len(nouns), 992)
+        eq_(len(nouns), 1485)
         ok_("time" in nouns)
 
     def test_blank_fill(self):
